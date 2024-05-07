e192fc26c6ef6ea856e0dc7db0a69376461cf769
Kharisma Agustya Zahra Salsabilla <142194971+kharistya281@users.noreply.github.com>
2024-05-07 13:45:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ data = pd.read_csv("tips.csv")
 st.title('Data Visualization of Tip Given by The Customer')
 
 st.write("Bar Chart")
-bar = plt.bar(data[['day', 'tip'])
-st.bar_chart(bar)
+st.bar_chart(data[['day', 'tip'])
 
 st.write("Line Chart")
 st.line_chart(data[['tip', 'size']])
